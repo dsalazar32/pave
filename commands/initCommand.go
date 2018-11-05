f97b6ed9fc7d35b70b454a1160fb75dabd726439
David N Salazar <dsalazar@iomediums.com>
2018-11-04 20:38:10 -0500

    Starting work around cli arguments for the initialization portion.

--- commands/initCommand.go
+++ commands/initCommand.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 type InitCommand struct {
@@ -17,9 +19,39 @@ type InitCommand struct {
 // - .(lang)-version
 // - Dockerfile
 func (c InitCommand) Run(args []string) int {
+	var (
+		prj string
+	)
+
 	cfg := Config{}
 	if err := cfg.Load(); err != nil {
+
+		// Assumption here is that the project has never been initialized.
+		// Interrogate to gather proper information for initialization.
 		if os.IsNotExist(err) {
+			f := flag.NewFlagSet("Init", flag.ContinueOnError)
+
+			// Set project name
+			// - Set via argument flag.
+			// - Ask and infer the default value being that of the directory
+			// that the project is in.
+			f.StringVar(&prj, "p", "", "project name")
+
+			// Set language that will be used in the project.
+			// - Set via argument flag.
+			// - Ask and infer from .<lang>-version file.
+			m, _ := filepath.Glob(".*-versions")
+			if len(m) == 0 {
+
+			} else {
+
+			}
+
+			if err := f.Parse(args); err != nil {
+				fmt.Printf("Error parsing arguments: %s", err)
+				return 1
+			}
+
 			if err := initializeProject(cfg); err != nil {
 				fmt.Printf("Something bad happened!: %v\n", err)
 				return 1
