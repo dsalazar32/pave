3cd219306081f2950a5794ae60220ffbb71db2f8
David Salazar <dsalazar@carecloud.com>
2018-11-21 09:43:27 -0500

    [card-14276854] (WIP) Initial project initialization logic.
    Adds flags and questions in the case that flags to:
    - Set project name
    - Set and validate supported project language and version
    
    Adds tests for project language validation
    
    https://github.com/dsalazar32/tinker/projects/1#card-14276854

--- commands/initCommand.go
+++ commands/initCommand.go
@@ -1,14 +1,33 @@
 package commands
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type InitCommand struct {
 	Meta
+}
+
+type langVers []string
+
+func (l langVers) String() string {
+	return ""
+}
+
+func (l *langVers) Set(value string) error {
+	lang := strings.Split(value, ":")
+	if len(lang) > 2 {
+		return errors.New("expected [language] or [language:vers]")
+	}
+	for _, lv := range lang {
+		*l = append(*l, lv)
+	}
+	return nil
 }
 
 // TODO: Check for .tinker directory and files
@@ -19,8 +38,11 @@ type InitCommand struct {
 // - .(lang)-version
 // - Dockerfile
 func (c InitCommand) Run(args []string) int {
+
 	var (
-		prj string
+		p    string
+		pl   langVers
+		lang Language
 	)
 
 	cfg := Config{}
@@ -35,37 +57,67 @@ func (c InitCommand) Run(args []string) int {
 			// - Set via argument flag.
 			// - Ask and infer the default value being that of the directory
 			// that the project is in.
-			f.StringVar(&prj, "p", "", "project name")
+			f.StringVar(&p, "p", "", "project name")
 
 			// Set language that will be used in the project.
 			// - Set via argument flag.
 			// - Ask and infer from .<lang>-version file.
-			m, _ := filepath.Glob(".*-versions")
-			if len(m) == 0 {
-
-			} else {
-
-			}
+			// m, _ := filepath.Glob(".*-versions")
+			// if len(m) == 0 {
+			// } else {
+			// }
+			f.Var(&pl, "l", "project language")
 
 			if err := f.Parse(args); err != nil {
-				fmt.Printf("Error parsing arguments: %s", err)
+				c.Ui.Error(fmt.Sprintf("Error parsing arguments: %s", err))
 				return 1
 			}
 
-			if err := initializeProject(cfg); err != nil {
-				fmt.Printf("Something bad happened!: %v\n", err)
+			// Configure Project
+			if len(p) == 0 {
+				pd, err := os.Getwd()
+				if err != nil {
+					fmt.Println(err)
+					return 1
+				}
+				pd = filepath.Base(pd)
+				p, _ = c.Ui.Ask(fmt.Sprintf("Project name? [%s]", pd))
+				if len(p) == 0 {
+					p = pd
+				}
+			}
+			cfg.ProjectName = p
+
+			// Configure Project Language
+			if len(pl) == 0 {
+				l, _ := c.Ui.Ask("Project language? [node]")
+				if len(l) == 0 {
+					l = "node"
+				}
+				lang = Language{name: l}
+			} else {
+				lang.Set(pl...)
+			}
+			if err := lang.Validate(); err != nil {
+				c.Ui.Error(err.Error())
 				return 1
 			}
-		} else {
-			fmt.Printf("Something bad happened!: %v\n", err)
-			return 1
+			cfg.Language = lang.String()
+
+			if err := initializeProject(cfg); err != nil {
+				c.Ui.Error(fmt.Sprintf("Something bad happened: %v\n", err))
+				return 1
+			}
 		}
+	} else {
+		c.Ui.Info("Tinker seems to be initialized in this project.\n" +
+			"To re-initialize the project run tinker init with the '-force' flag or remove the .tinker directory.")
 	}
 	return 0
 }
 
 func initializeProject(cfg Config) error {
-	panic("implement!")
+	fmt.Printf("%v\n", cfg)
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/dsalazar32/tinker
 
 require github.com/mitchellh/cli v1.0.0
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/coreos/go-semver v0.2.0
+	gopkg.in/yaml.v2 v2.2.1 // indirect
+)
